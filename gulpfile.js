3a615609833c83d2de66ef8e3694e51be7302e46
Umed Khudoiberdiev <zarrhost@gmail.com>
2016-01-18 01:27:20 +0500

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,4 @@
-eval(require('typescript').transpile(require('fs').readFileSync('./gulpfile.ts').toString()));
+// normally you'll need to do this:
+// eval(require("typescript").transpile(require("fs").readFileSync("./gulpfile.ts").toString()));
+// but this case is a bit different and alternative approach is used there
+require('./build/es5/gulpfile');
